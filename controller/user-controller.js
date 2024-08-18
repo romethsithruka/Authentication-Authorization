d79544418eb3cb7e2e89442b292d7cebebb10356
rometh sithruka gunathilaka <romethsithruka6@gmail.com>
2024-08-19 03:03:08 +0530

    railway

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,5 +1,8 @@
 const User = require("../model/user");
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+const user = require("../model/user");
+const JWT_SECRET_KEY = "my key";
 const signup = async (req, res, next) => {
   const { name, email, password } = req.body;
   let existingUser;
@@ -41,8 +44,33 @@ const login = async (req,res,next) =>{
     if (!isPasswordCorrect){
         return res.status(400).json({message: 'Invalid Email / Password'})
     }
-    return res.status(200).json({message:'Successfully Logged In'})
-}
+    const token = jwt.sign({id: existingUser._id},JWT_SECRET_KEY,{
+      expiresIn:"1hr"
+    });
+    return res
+    .status(200)
+    .json({message:'Successfully Logged In',user:existingUser,token})
+};
+const verifyToken = (req,res,next)=>{
+  const headers = req.headers['authorization'];
+  const token = headers.split('')[1];
+  if(!token){
+    res.status(404).json({message: "No token found"})
+  }
+  jwt.verify(String(token),JWT_SECRET_KEY,(err, user)=>{
+    if(err){
+     return res.status(400).json({message:"invalid Token"})
+    }
+console.log(user.id);
+req.id = user.id;
+  });
+  next();
+};
+
+
+
+
 
 exports.signup = signup;
 exports.login = login;
+exports.verifyToken = verifyToken;
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { signup, login } = require("../controller/user-controller");
+const { signup, login, verifyToken } = require("../controller/user-controller");
 
 const router = express.Router();
 
 router.post("/signup",signup);
 router.post("/login",login);
+router.get("/user",verifyToken);
 
 module. exports = router
